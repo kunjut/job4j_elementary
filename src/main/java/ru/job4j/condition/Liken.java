f84f5fab82ffd032ff7ad450426c61ac35724b8c
Vadim Dimnich <v.dimnich@gmail.com>
2022-03-22 18:47:37 +0300

    0. Операции сравнения [#235631]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = true;
+        System.out.println(result);
+
+        result = false;
+        System.out.println(result);
+
+        result = false;
+        System.out.println(result);
+    }
+}
